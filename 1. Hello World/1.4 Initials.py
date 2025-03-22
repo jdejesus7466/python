cffb18b59cd3355992a21aa8992deeeecbf817d5
Joseph De Jesus <jdejesus7466@gmail.com>
2025-03-22 14:51:48 -0700

    Chapter 1 Python

---
+++ 1. Hello World/1.4 Initials.py
@@ -0,0 +1,8 @@
+if __name__ == "__main__":
+    print("JJJJJJJJJJJJ  DDDDDD")
+    print("      J       D     D")
+    print("      J       D     D")
+    print("      J       D     D")
+    print("J     J       D     D")
+    print(" J    J       D     D")
+    print("  JJJJ        DDDDDD")
---
+++ 1. Hello World/1.5 Snail Mail.py
@@ -0,0 +1,6 @@
+if __name__ == "__main__":
+    print("Today's date: 3/22/2025")
+    print("How I'm Feeling: Amazing")
+    print("What I want to Accomplish: I want to be interview ready and have a good portfolio")
+    print("Dear Older Me: Don't Ever Give up, You've Got this!")
+    print("Emoji: 😼")
